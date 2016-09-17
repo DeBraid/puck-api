6f954578518042ef6fb199f13b32a6f58a2638c1
debraid <de.braid@gmail.com>
2016-09-17 10:54:31 -0400

    cleaner routing for skater stats

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var bodyParser  = require('body-parser');
 var app = express();
-var goals = require('./skaters-goals.js');
-var corsi = require('./skaters-corsi.js');
+// var goals = require('./skaters-goals-201516.js');
+// var corsi = require('./skaters-corsi-201516.js');
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -21,21 +21,14 @@ app.get('/', function (req, res) {
     res.send('Hello World from app.js of PUCK-API! ');
 });
 
-// app.get('/skaters', function(req, res) {
-//   res.json(skaters);
+app.get('/skaters/:stat_type', function(req, res) {
+  var data_type = require('./skaters-'+req.params.stat_type+'-201516.js');
+  res.json(data_type);
+});
+
+// app.get('/skaters/:season', function(req, res) {
+//   var q = req.params.season;
+//   res.json(skaters[q]);
 // });
-
-app.get('/skaters/goals', function(req, res) {
-  res.json(goals);
-});
-
-app.get('/skaters/corsi', function(req, res) {
-  res.json(corsi);
-});
-
-app.get('/skaters/:season', function(req, res) {
-  var q = req.params.season;
-  res.json(skaters[q]);
-});
 
 app.listen(process.env.PORT || 4730);
